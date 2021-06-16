b4deaaf5ef6a75c1ee35e9ef510a97f6b194c9b5
GreenHairedDude <75127118+Greeny127@users.noreply.github.com>
2021-06-16 22:06:33 +0530

    Fixed bugs, finished AZERTY layout

--- scripts/options.js
+++ scripts/options.js
@@ -3,12 +3,40 @@ $(function()
       $('[name="layouts"]').change(function()
       {
         if ($(this).val() == "2") {
-           console.log("2");
+           azerty();
         }
 
         else{
-            console.log("1");
+            qwerty();
         };
 
       });
 });
+
+function azerty(){
+    $('#Q').html("A");
+    $('.keyQ').addClass('keyA').removeClass('keyQ');
+
+    $('#W').html("Z");
+    $('.keyW').addClass('keyZ').removeClass('keyW');
+
+    $('#A').html("Q");
+    $('#A').addClass('keyQ').removeClass('keyA');
+
+    $('#Z').html("W");
+    $('#Z').addClass('keyW').removeClass('keyZ');
+}
+
+function qwerty(){
+    $('#Q').html("Q");
+    $('#Q').addClass('keyQ').removeClass('keyA');
+
+    $('#Z').html("Z");
+    $('#Z').addClass('keyZ').removeClass('keyW');
+
+    $('#A').html("A");
+    $('#A').addClass('keyA').removeClass('keyQ');
+
+    $('#W').html("W");
+    $('#W').addClass('keyW').removeClass('keyZ');
+}
